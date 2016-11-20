be2f449acbac620f76c934c757c9be1451ec08af
Vincent Dubois <dubois.vct@free.fr>
2016-11-20 11:36:39 +0100

    No need to autodiscover config server since we specify it

--- src/main/java/io/github/vdubois/SpringCloudSampleConfigServerApplication.java
+++ src/main/java/io/github/vdubois/SpringCloudSampleConfigServerApplication.java
@@ -3,11 +3,9 @@ package io.github.vdubois;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class SpringCloudSampleConfigServerApplication {
 
 	public static void main(String[] args) {
